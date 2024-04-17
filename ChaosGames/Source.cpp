ccc9f5a477d535b884d10507aac5f9263fc4d095
Donaldson10 <166642140+Donaldson10@users.noreply.github.com>
2024-04-17 09:26:01 -0700

    Update Source.cpp
    changed size of points

--- ChaosGames/Source.cpp
+++ ChaosGames/Source.cpp
@@ -163,13 +163,13 @@ int main()
                 //for (int i = 0; i < 15; i++) {
                     for (int i = 0; i < vertices.size(); i++)
                     {
-                        RectangleShape rect(Vector2f(1, 1));
+                        RectangleShape rect(Vector2f(5, 5));
                         rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
                         rect.setFillColor(Color::Blue);
                         window.draw(rect);
                     }
                     for (int i = 0; i < points.size(); i++) {
-                        RectangleShape rect(Vector2f(1, 1));
+                        RectangleShape rect(Vector2f(5, 5));
                         rect.setPosition(Vector2f(points[i].x, points[i].y));
                         rect.setFillColor(Color::Blue);
                         window.draw(rect);
